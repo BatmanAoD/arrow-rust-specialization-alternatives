9bd3950dc8118bc98e5a3c8d07590d13e81f27e5
Kyle Strand <kyle.strand@pieinsurance.com>
2020-09-12 21:33:11 -0600

    Begin writing tests

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,36 @@ mod bit_util {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn int_index() {
+        let backing = [0, 0, 0, 42, 0];
+        let arr = PrimitiveArray::<TestInt>{ raw_values: &backing[0] as *const i32 };
+        assert!(arr.value(2) == 0);
+        assert!(arr.value(3) == 42);
+    }
+
+    /* XXX how is the bool-pointer created for a bit-packed boolean array?
+    #[test]
+    fn bool_index() {
+        let backing = 4;
+        let arr = PrimitiveArray::<BooleanType>{ raw_values: &backing as *const bool };
+        assert!(arr.value(2) == false);
+        assert!(arr.value(3) == true);
+    }
+    */
+
+    struct TestInt {}
+    impl ArrowPrimitiveType for TestInt {
+        type Native = i32;
+    }
+    impl ArrowNumericType for TestInt {}
+}
+
+
 trait ArrowPrimitiveType {
     type Native: Copy;
 }
@@ -17,6 +47,7 @@ trait ArrowNumericType: ArrowPrimitiveType {}
 
 struct BooleanType {}
 impl ArrowPrimitiveType for BooleanType {
+    // XXX is this correct? It seems... not correct.
     type Native = bool;
 }
 
